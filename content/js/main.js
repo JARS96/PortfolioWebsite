457fe9c233e2c6067dcf78b8a03903213224a763
JARS <J.ARS96@outlook.com>
2022-07-10 17:30:13 -0500

    Portfolio completed

--- content/js/main.js
+++ content/js/main.js
@@ -96,6 +96,20 @@ modalCloses.forEach((modalClose)=>{
 /*==================== PORTFOLIO SWIPER  ====================*/
 
 
+let swiper = new Swiper('.portfolio_container', {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
 /*==================== TESTIMONIAL ====================*/
 
 
